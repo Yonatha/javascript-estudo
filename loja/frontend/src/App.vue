571ed8d964db896a490be0960f2a0daac2253da4
Yonatha <yonathalmeida@gmail.com>
2023-10-07 17:36:47 -0300

    Ajuste no layout do formulario e listagem de clientes

--- loja/frontend/src/App.vue
+++ loja/frontend/src/App.vue
@@ -1,17 +1,10 @@
 <template>
-
-  <div style="width: 25%; float: left;">
-    <CadastrarCliente />
-  </div>
-
-  <div style="width: 25%; float: left;">
-    <ListarCliente />
-  </div>
+  <CadastrarCliente />
+  <ListarCliente />
 
   <div style="width: 25%; float: left;">
     <CadastrarFornecedor />
   </div>
-
 </template>
 
 <script setup>
@@ -19,3 +12,9 @@ import CadastrarCliente from "./components/clientes/CadastrarCliente.vue"
 import ListarCliente from "./components/clientes/ListarCliente.vue"
 import CadastrarFornecedor from "./components/fornecedores/CadastrarFornecedor.vue"
 </script>
+
+<style>
+html {
+  font-family: Arial;
+}
+</style>
--- loja/frontend/src/components/clientes/CadastrarCliente.vue
+++ loja/frontend/src/components/clientes/CadastrarCliente.vue
@@ -1,35 +1,36 @@
 <template>
-    <h3>Cadastro de cliente</h3>
-    <p>
-        {{ notificacao }}
-    </p>
-    <label>Nome</label>
-    <br>
-    <input name="nome" v-model="cliente.nome" /><br>
-  
-    <label>E-mail</label><br>
-    <input name="email" v-model="cliente.email" /><br>
-  
-    <label>CPF</label><br>
-    <input name="cpf" v-model="cliente.cpf"/><br>
-  
-    <label>CEP</label><br>
-    <input name="cep" v-model="cliente.cep" v-on:blur="buscarCep()"/><br>
-  
-    <label>Endereco</label><br>
-    <input name="endereco" v-model="cliente.endereco"/><br>
-  
-    <label>UF</label><br>
-    <input name="uf" v-model="cliente.uf"/><br>
-  
-    <label>Complemento</label><br>
-    <input name="complemento" v-model="cliente.complemento"/><br><br>
-  
-    <button @click="cadastrar()">
-            Cadastrar
-    </button>
-  </template>
-
+    <div class="formularioClientes">
+        <h3>Cadastro de cliente</h3>
+        <p>
+            {{ notificacao }}
+        </p>
+        <label>Nome</label>
+        <input name="nome" v-model="cliente.nome" />
+        
+        <label>E-mail</label>
+        <input name="email" v-model="cliente.email" />
+    
+        <label>CPF</label>
+        <input name="cpf" v-model="cliente.cpf"/>
+    
+        <label>CEP</label>
+        <input name="cep" v-model="cliente.cep" v-on:blur="buscarCep()"/><br>
+    
+        <label>Endereco</label>
+        <input name="endereco" v-model="cliente.endereco"/>
+    
+        <label>UF</label>
+        <input name="uf" v-model="cliente.uf"/>
+    
+        <label>Complemento</label>
+        <input name="complemento" v-model="cliente.complemento"/>
+    
+        <button @click="cadastrar()">
+                Cadastrar
+        </button>
+    </div>
+</template>        
+    
 <script>
 
 import axios from 'axios'
@@ -79,3 +80,39 @@ export default {
     }
 }
 </script>
+
+<style>
+.formularioClientes
+ {
+  padding: 10px;
+  border:  1px solid rgb(108, 106, 106);
+  float: left;
+}
+
+.formularioClientes {
+  width: 25%; 
+  background: #efeeee;
+}
+
+.formularioClientes input {
+    width: 96%;
+    margin-bottom: 10px;
+    font-size: 14px;
+    padding: 5px;
+}
+
+.formularioClientes label {
+  width: 80%;
+  display: block;
+  margin-right: 5px;
+  text-transform: uppercase;
+}
+
+.formularioClientes button {
+  background-color: aquamarine;
+  border: none;
+  float: right;
+  width: 92px;
+  height: 26px;
+}
+</style>
