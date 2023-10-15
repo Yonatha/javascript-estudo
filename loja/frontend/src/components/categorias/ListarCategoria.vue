8343b80b9afae78dd873ad791bc25231f9ec01e6
Samara Raquel <samara.raquelmoura@gmail.com>
2023-10-14 22:29:56 -0300

    63 Listar Categorias Cadastrada

--- loja/frontend/src/components/categorias/ListarCategoria.vue
+++ loja/frontend/src/components/categorias/ListarCategoria.vue
@@ -3,14 +3,7 @@
     {{ mensagem }}
     <ul>
         <li v-for="categoria in categorias">{{ categoria.nome }}</li>
-        <li v-for="categoria in categorias">
-            {{ categoria.id }}
-            {{ categoria.nome }}
 
-            <a @click="deletar(categoria.id)" href="#">
-                Excluir
-            </a>
-        </li>
     </ul>
 </template>
 
@@ -39,11 +32,7 @@ export default {
             const responce = await minhaApi.get("/categoria/")
             this.categorias = responce.data
         
-        },
-        async deletar(id) {
-            const responce = await minhaApi.delete(`/categoria/${id}`)
-            this.mensagem = responce.data
-            this.listar()
+        
     
        }
     }
