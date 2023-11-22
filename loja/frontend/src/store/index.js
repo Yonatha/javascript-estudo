355861fb4810b9552ac8d805150e2280e9167256
Samara Raquel <samara.raquelmoura@gmail.com>
2023-11-22 20:53:49 -0300

    atualização da feature 95

--- loja/frontend/src/store/index.js
+++ loja/frontend/src/store/index.js
@@ -2,11 +2,13 @@ import {createStore} from 'vuex'
 import carrinho from "./modules/carrinho/carrinho.module"
 import fornecedor from "./modules/fornecedor/fornecedor.module"
 import notificacao from "./modules/notificacao/notificacao.module"
+import categoria from "./modules/categoria/categoria.module"
 
 export default createStore({
     modules: {
         carrinho,
         fornecedor,
-        notificacao
+        notificacao,
+        categoria
     }
 })
