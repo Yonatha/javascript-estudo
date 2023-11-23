f4f6598cb1ee20a823fed1f66b483f80d0326412
Yonatha <yonathalmeida@gmail.com>
2023-11-23 15:37:14 -0300

    formas de pagamento e QR code para PIX

--- loja/frontend/src/components/carrinhos/Compra.vue
+++ loja/frontend/src/components/carrinhos/Compra.vue
@@ -1,23 +1,54 @@
 <template>
-        <h3>Compra</h3>
+    <h3>Compra</h3>
+
+    <h4>Escolha a forma de pagmaento</h4>
+    <input type="radio" v-model="formaPagamento" v-on:change="alterarFormaDePagamento" name="forma_pagamento" value="cc" /> Cartão<br>
+
+    <input type="radio" v-model="formaPagamento" v-on:change="alterarFormaDePagamento" name="forma_pagamento" value="pix" /> PIX<br>
+
+    <input type="radio" v-model="formaPagamento" v-on:change="alterarFormaDePagamento" name="forma_pagamento" value="boleto" /> Boleto<br>
+
+    <img :src="qrCode" v-show="formaPagamento == 'pix'" />
+
 </template>
 
 <style scoped>
 </style>
 
 <script>
-import { mapGetters, mapActions, useStore } from 'vuex';
+import { mapGetters, mapActions, useStore } from 'vuex'
+import QRCode from 'qrcode'
+
 export default {
     name: "Compra",
-    setup: () => {
-        const store = useStore()
-        store.dispatch("carrinho/listar")
+    data(){
+    return {
+            formaPagamento: null,
+            qrCode: null
+        }
     },
     computed: {
         ...mapGetters("carrinho", ["produtos", "total"]),
     },
     methods: {
-        ...mapActions("carrinho", ["removerProduto", "finalizarCompra"])
-    }
+        alterarFormaDePagamento() {
+            if (this.formaPagamento === 'pix') {
+                this.pix();
+            } else {
+
+            }
+        },
+        pix() {
+            QRCode.toDataURL('I am a pony!', {
+                width: 300,
+                height: 300,
+                margin: 1
+            }, async (err, url) => {
+                this.qrCode = await url
+            })
+        }
+    },
+    async mounted() {
+    },
 }
 </script>
