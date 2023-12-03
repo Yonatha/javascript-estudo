29b5e2b6378355c5fbacedaff5a703cbdfd13232
Samara Raquel <samara.raquelmoura@gmail.com>
2023-12-03 17:31:08 -0300

    atualização

---
+++ loja/backend/Controller/CheckoutController.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const router = express.Router()
+
+import {
+    exibirCliente
+  } from '../services/CheckoutService.js'
+
+router.get('/:id', async function (request, responce) {
+    const id = request.params.id
+    const cliente = await exibirCliente(id)
+    responce.send(cliente)
+  })
