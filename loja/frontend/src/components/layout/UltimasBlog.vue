bd737042099f18b5ebd3196e91502440aaa56ebe
Fábio <fabiomirandamonte@gmail.com>
2024-01-08 09:06:21 -0300

    Atualização

--- loja/frontend/src/components/layout/UltimasBlog.vue
+++ loja/frontend/src/components/layout/UltimasBlog.vue
@@ -1,15 +1,41 @@
 <template>
-    <div id="post1">
-        <img src="/images/post1.png" class="imagem-post1"/>
+    <div class="container">
+        <div class="imagem">
+            <img src="images/post1.png" alt="post1">
+            <p>Titulo Post 1</p>
+        </div>
+        <div class="imagem">
+            <img src="images/post2.png" alt="post2">
+            <p>Titulo Post 2</p>
+        </div>
+        <div class="imagem">
+            <img src="images/post3.png" alt="post3">
+            <p>Titulo Post 3</p>
+        </div>
     </div>
+
 </template>
 
-<style scoped>
-.imagem-post1 {
-    width: 350px;
-    height: auto;
-    margin-left: 87px;
-    margin-top: 87px;
-    text-align: left;
+<style>
+.container {
+    display: inline-flex;
+    justify-content: center;
+    max-width: 1200px;
+    margin: 0 auto;
 }
+
+.imagem {
+    flex: 0 0 calc(33.33% - 20%);
+    margin: 169px;
+    text-align: center;
+}
+
+.imagem img {
+    max-width: 200%;
+}
+
+.imagem p {
+    text-align: center;
+}
+
 </style>
