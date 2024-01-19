5aa18467ac01102b3f0ce9aaafa97518651a1024
Fábio <fabiomirandamonte@gmail.com>
2024-01-19 19:23:49 -0300

    atualização

--- loja/frontend/src/components/layout/Rodape.vue
+++ loja/frontend/src/components/layout/Rodape.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="rodape">
-        <img src="images/webchain-rodape.png" alt="webchain" class="webchain-rodape">
-        
-        <footer>
-            <a href="#" class="endereco">
-                Av. Nome completo da Avenida, 8007,<br>
-                Bairro, Cidade, Estado
-            </a>
-            <a href="#" class="email">
-                E-mail: info@nomeloja.com.br
-            </a>
-            <a href="#" class="telefone">
-                Telefone: +55 83 9999-9999
-            </a>
-        </footer>
-        <div class="informacao"> 
-            <p>INFORAMÇÃO</p>
+        <div class="conteudo">
+            <footer>
+                <img src="images/webchain-rodape.png" alt="webchain" class="webchain-rodape">
+                    
+                <a href="#" class="endereco">
+                    Av. Nome completo da Avenida, 8007,<br>
+                    Bairro, Cidade, Estado
+                </a>
+                <a href="#" class="email">
+                    E-mail: info@nomeloja.com.br
+                </a>
+                <a href="#" class="telefone">
+                    Telefone: +55 83 9999-9999
+                </a>
+            </footer>
+
+            <div class="informacao"> 
+                <p>INFORAMÇÃO</p>
+            </div>
+            <div class="conta"> 
+                <p>MINHA CONTA</p>
+                <a href="#" class="email">
+                    Perfil
+                </a>
+                <a href="#" class="Login">
+                    Login
+                </a>
+
+            </div>
+            <div class="newslatter"> 
+                <p>informe seu e-mail</p>
+                <p>SIGA NAS REDES SOCIAIS</p>
+
+            </div>
         </div>
     </div>
 
@@ -30,17 +48,22 @@
     border: red 1px solid;
     margin: auto;
     text-align: left; 
-    background-color: black;  
+    background-color: black;
+    justify-content: space-between; 
 }
 
-.informacao {
+.conteudo {
+    display: flex;
+    align-items: flex-start;
+}
+
+.informacao, .conta, .newslatter {
     width: 149px;
     height: 176px;
     border: yellow 1px solid;
     margin: auto;
     text-align: left;
-    background-color: darkgray;
-
+    background-color: darkgray;    
 }
 
 .webchain-rodape {
@@ -52,6 +75,7 @@
     display: block;
     padding: 20px;
     height: 15px;
+    width: 235px;
     margin: 0px;
     color: white;
     margin-left: 83px;
