eee6d5417e2a2f9f6c533a3f18826f1cfd5c07ca
Yonatha <yonathalmeida@gmail.com>
2024-02-04 16:54:32 -0300

    feature/116 carregar produtos promocionais do banco de dados

---
+++ loja/frontend/src/components/layout/Promocoes/PromocaoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="box">
+        <a href="#">{{ nome }}</a>
+        <span class="desconto">R$ {{ desconto }}</span>
+        <span>R$ {{ valor }}</span>
+    </div>
+</template>
+
+
+<style></style>
+
+
+<script>
+export default {
+    props: ['nome','desconto','valor'],
+}
+</script>
